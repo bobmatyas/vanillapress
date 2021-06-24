79f2d4e178fc4100fb6e136bb8c2e63d82bb8608
bobmatyas <45246438+bobmatyas@users.noreply.github.com>
2021-06-24 14:10:08 -0400

    idisplay list of blog posts from json

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ var vanillaPress = {
     // Add any functions here you want
     // to run to start the application
     console.log( jsonData );
-
+    var posts = JSON.parse(jsonData);
   }
 
 };
@@ -23,3 +23,36 @@ var vanillaPress = {
 vanillaPress.init();
 
 // Add your custom code starting here:
+
+console.log(posts);
+
+for ( var i = 0; i < posts.length; i++ ) {
+  displayPost( posts[i].id, posts[i].title, posts[i].content) ;
+}
+
+function displayPost ( id, title, content ) {
+  var postContainer = document.createElement('article');
+  postContainer.setAttribute( 'id', postId( id ) );
+  postContainer.appendChild( displayPostTitle( title ) );
+  postContainer.appendChild( displayPostBody( content ) );
+  document.getElementById( 'pageContent' ).appendChild( postContainer );
+}
+
+function displayPostTitle ( title ) {
+  var postTitle = document.createElement( 'h2' );
+  postTitle.innerText = title;
+  return postTitle;
+}
+
+function displayPostBody ( content ) {
+  var postContent = document.createElement( 'div' );
+  postContent.innerHTML = content;
+  return postContent
+}
+
+function postId ( id ) {
+  return `id${id}`
+}
+
+
+
