acecb99a2e20c2bc53c1179222a84f82a8043787
bob <45246438+bobmatyas@users.noreply.github.com>
2021-06-24 19:11:30 -0400

    setup home link

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ var vanillaPress = {
     // to run to start the application
     console.log( jsonData );
     var posts = JSON.parse(jsonData);
+    showPosts( posts );
   }
 
 };
@@ -24,10 +25,27 @@ vanillaPress.init();
 
 // Add your custom code starting here:
 
-console.log(posts);
+//console.log(posts);
 
-for ( let i = 0; i < posts.length; i++ ) {
-  displayPost( posts[i].id, posts[i].title, posts[i].content, posts[i].slug ) ;
+/* home link */
+document.querySelector( '#siteName a' ).addEventListener( 'click', function(event) {
+    event.preventDefault();
+    clearPosts();
+    showPosts( posts );
+  });
+
+
+function showPosts( posts ) {
+  for ( let i = 0; i < posts.length; i++ ) {
+    displayPost( posts[i].id, posts[i].title, posts[i].content, posts[i].slug ) ;
+  }
+}
+
+function clearPosts() {
+  const posts = document.querySelectorAll( 'article' );
+  for ( let i = 0; i < posts.length; i++ ) {
+    document.getElementById( posts[i].id ).remove();
+  }
 }
 
 function displayPost ( id, title, content, slug ) {
