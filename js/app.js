0a3b944ddcf9fb356c4e22aca43423b129cb2b80
bob <45246438+bobmatyas@users.noreply.github.com>
2021-06-24 19:37:28 -0400

    display individual posts

--- js/app.js
+++ js/app.js
@@ -48,27 +48,41 @@ function clearPosts() {
   }
 }
 
-function displayPost ( id, title, content, slug ) {
+function displayPost( id, title, content, slug ) {
   const postContainer = document.createElement('article');
   postContainer.setAttribute( 'id', postId( id ) );
-  postContainer.appendChild( displayPostTitle( title, slug ) );
+  postContainer.appendChild( displayPostTitle( id, title, content, slug ) );
   postContainer.appendChild( displayPostBody( content ) );
   document.getElementById( 'pageContent' ).appendChild( postContainer );
 }
 
-function displayPostTitle ( title, slug ) {
+function displayPostTitle( id, title, content, slug ) {
   const postTitle = document.createElement( 'h2' );
   const postLink = document.createElement( 'a' );
   postLink.setAttribute( 'href', hashLink( slug ) )
+  postLink.addEventListener( 'click', (e) => displaySinglePost( e, id, title, content, slug ));
   postLink.innerText = title;
   postTitle.appendChild( postLink );
   return postTitle;
 }
 
-function displayPostBody ( content ) {
+function displayPostBody( content ) {
   const postContent = document.createElement( 'div' );
   postContent.innerHTML = content;
   return postContent
+}
+
+function displaySinglePost( e, id, title, content, slug ) {
+  e.preventDefault();
+  clearPosts();
+  displayPost( id, title, content, slug );
+
+}
+
+function displaySinglePostTitle( title ) {
+  const postTitle = document.createElement( 'h2' );
+  postTitle.innerText = title;
+  return postTitle;
 }
 
 function postId ( id ) {
